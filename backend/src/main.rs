9efee22e76cd6ef692667bd4f70d07a82359e3f8
SarvilRathour <ken.kaneki.260905@gmail.com>
2025-09-09 11:56:08 -0400

    defined endpoints for api

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,20 @@
 use axum::{
-    routing::get,
+    routing::{get,post},
     Router,
 };
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-
+    let app = Router::new().route("/ev-request", post())   // User books a charging slot. 
+                            .route("/ev-requests",get()) //List all bookings.
+                            .route("/energy-status", get()) //Return solar/wind/battery/grid availability (dummy or real API).
+                            .route("/charging-plan", get()) // Run optimization engine and return schedule.
+                            .route("/analytics", get()); //Cost savings, renewable usage, CO₂ avoided.
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
+
+
+
