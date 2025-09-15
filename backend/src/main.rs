57f4bed4d827a9f035abd251ab57b1453e886f1d
ken-kaneki-9 <160580260+ken-kaneki-9@users.noreply.github.com>
2025-09-15 12:07:01 +0530

    Add DB connection and automatic migrations setup

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,9 +3,20 @@ use std::net::SocketAddr;
 
 mod routes;
 mod handlers;
-
+mod db;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    // load DATABASE_URL (from env or .env)
+    let database_url = std::env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+
+    // create pool
+    let pool = db::create_pool(&database_url).await?;
+
+    // 🔹 Run migrations automatically
+    sqlx::migrate!("./migrations")
+        .run(&pool)
+        .await?;
     let app: Router = routes::router();
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
 
--- backend/src/routes/analytics.rs
+++ backend/src/routes/analytics.rs
@@ -1,13 +1,1 @@
-#[derive(Debug,Clone)]
-pub struct EVpayload{
-  pub ev_id:String,
-  pub arrival_time:String,
-  pub duration:u16,
-  pub power_needed:i16,
-}
-#[derive(Debug,Clone)]
-pub struct EVresponse{
-  pub status:String,
-  pub slot_id:String,
-  pub message:String,
-}
+
