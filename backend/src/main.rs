98ed694dadf7a93ec6ec99a13a4080470d894807
ken-kaneki-9 <160580260+ken-kaneki-9@users.noreply.github.com>
2025-09-17 12:42:49 +0530

    Added shared state so all handlers can access the shared state

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,6 +4,13 @@ mod db;
 mod handlers;
 mod routes;
 use dotenvy::dotenv;
+use sqlx::PgPool;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub db_pool: PgPool,
+}
+use std::sync::Arc;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // load DATABASE_URL (from env or .env)
@@ -12,10 +19,10 @@ async fn main() -> anyhow::Result<()> {
 
     // create pool
     let pool = db::create_pool(&database_url).await?;
-
+    let shared_state = Arc::new(AppState { db_pool: pool.clone() });
     // 🔹 Run migrations automatically
     sqlx::migrate!("./migrations").run(&pool).await?;
-    let app: Router = routes::router();
+    let app: Router = routes::router(shared_state.clone());
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
 
     // Use tokio + hyper directly
--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -2,22 +2,23 @@ pub mod analytics;
 pub mod booking;
 pub mod energy;
 pub mod plan;
-
+use sqlx::PgPool;
 use axum::{
     Router,
     routing::{get, post},
 };
-
+use crate::AppState;
+use std::sync::Arc;
 mod super_handlers {
     // re-export handlers from crate root handlers module
     pub use crate::handlers::{analytics, charging_plan, energy_status, ev_request, ev_requests};
 }
-
-pub fn router() -> Router {
+pub fn router(state: Arc<AppState>) -> Router {
     Router::new()
         .route("/ev-request", post(super_handlers::ev_request))
         .route("/ev-requests", get(super_handlers::ev_requests))
         .route("/energy-status", get(super_handlers::energy_status))
         .route("/charging-plan", get(super_handlers::charging_plan))
         .route("/analytics", get(super_handlers::analytics))
+        .with_state(state)
 }
